2cb8b1469d262bb11223a627b0e2d1b0fdbcde34
Jonathan Tuzman <tuzmusic@gmail.com>
2018-11-27 14:12:29 -0500

    write landmark_controller (before tests)

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,4 +1,47 @@
 class LandmarksController < ApplicationController
 
+    
+  # Index action
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :'landmarks/index'
+  end
+  
+  # Create action
+  get '/landmarks/new' do
+    @titles = Title.all
+    @figures = Figure.all
+    erb :'landmarks/new'
+  end
+
+  post '/landmarks' do
+    landmark = Landmark.new(params[:landmark])
+    landmark.figure = Figure.create(name: params[:figure][:name]) unless params[:figure][:name].empty?
+    landmark.save
+  end
+
+  # Show action
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    erb :"landmarks/show"
+  end
+
+  # Edit action
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find(params[:id])
+    @figures = Figure.all
+    erb :"landmarks/edit"
+  end
+
+  patch '/landmarks/:id' do
+    landmark = Landmark.find(params[:id])
+    
+    landmark.update(params['landmark'])
+    landmark.figure = Figure.create(name: params['figure']['name']) unless params['figure']['name'].empty?
+    
+    redirect "/landmarks/#{landmark.id}"
+  end
+
+
  
 end
